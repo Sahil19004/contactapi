d490e3ca4f4badd0c29bd5ce53a3b6e5134224cb
Sahil Kumar <165409654+Sahil19004@users.noreply.github.com>
2025-09-05 18:17:37 +0530

    Update settings.py

--- contactapi/settings.py
+++ contactapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%zk*)xe=@$2f%j&b!^)ncxonox)whe%+lh6&%+g8!1vz&z=e%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,4 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
